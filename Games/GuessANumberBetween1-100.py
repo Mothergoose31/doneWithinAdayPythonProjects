136406331475407d84334f879bda06a8000b102d
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-03-19 11:32:07 -0700

    number guess

---
+++ Games/GuessANumberBetween1-100.py
@@ -0,0 +1,16 @@
+import random
+# Module implements the Mersenne Twister random
+# number generator.
+
+
+# Create a number guessing game, gives users a certain ammount of guesses
+#  if the guess is higher than the  number let use know 
+# that they  should guess lower, if  number is higher than guess ,
+#  let user know that they need to guess lower
+# if guess == number  let user know they have won, if user run out of guesses , 
+# let user know that they have lost 
+
+
+
+
+
