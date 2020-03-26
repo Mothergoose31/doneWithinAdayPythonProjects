81308f6f5e1a519abe8cc6ea1931d7703347eb81
Nata <JosephAlvarengaBeech@protonmail.com>
2020-03-25 18:21:22 -0700

    passwordGenerator

--- PracticalProejcts/StopWatch.py
+++ PracticalProejcts/StopWatch.py
@@ -1,4 +1,15 @@
-import readchar
+# import readchar
 
-print("up and running!")
-key = readchar.readkey()
+
+# print("up and running!")
+# key = readchar.readkey()
+
+
+import time
+
+input("Press Enter to start")
+start_time = time.time()
+input("Press Enter to stop")
+end_time = time.time()
+time_lapsed = end_time - start_time
+print(time_lapsed, "seconds have passed")
