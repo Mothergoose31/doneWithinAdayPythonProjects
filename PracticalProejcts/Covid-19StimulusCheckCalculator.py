3c410c2979f4db27120ffb404b3a9e2927d6758e
Nata <JosephAlvarengaBeech@protonmail.com>
2020-04-10 15:48:12 -0700

    finished calculator

--- PracticalProejcts/Covid-19StimulusCheckCalculator.py
+++ PracticalProejcts/Covid-19StimulusCheckCalculator.py
@@ -1,7 +1,7 @@
 MaxCashForSingle= 1200
 MaxCashFormarried = 2400
 
-filling_status = str(input('Enter your filling status,  S for single or M for married: '))
+filling_status = int(input('Enter your filling status,  S for single or M for married: ')).capitalize
 
 income = int(input('Enter your 2018 Federal Income: '))
 
@@ -15,5 +15,24 @@ def stimulusCheck(*args):
     else:
         print('your stimulus check will mostlikely be: ',round(MaxCashFormarried -((income - 150000)/100)+CashForChildren))
 
+def maximum_posible_stimulusCheck(*args):
+    if filling_status == 'S':
+        print('Your stimulus check will most likely be :',MaxCashForSingle + CashForChildren)
+    else:
+        print('Your stimulus check will most likely be :',MaxCashFormarried + CashForChildren)
+
+
+# if you don't qualify for the stimulus check but have children
+def minimum_stimulusCheck(*args):
+    print('your stimulus check will mostlikely be:', CashForChildren)
+
+if (filling_status == 'S' and income > 99000) or (filling_status == 'M' and income > 198000):
+        minimum_stimulusCheck()
+
+elif (filling_status == 'S' and income > 75000) or (filling_status == 'M' and income > 150000):
+        stimulusCheck()
+
+else:
+    maximum_posible_stimulusCheck()
 
 
